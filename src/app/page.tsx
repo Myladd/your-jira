8408a7edd6602cae2972890c1173725840596330
Myladd <mylad.zandkarimi@gmail.com>
2024-11-05 17:05:57 +0330

    implement logout users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return (
+    <div>
+      Only Users Auth
+      <Button onClick={() => mutate()}>logout</Button>
+    </div>
+  );
 }
